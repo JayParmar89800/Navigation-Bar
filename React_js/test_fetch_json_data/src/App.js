b3dfe912e387d9c6c3b392ce5405be3088b16d69
JayParmar89800 <jayp89800@gmail.com>
2023-11-01 19:15:48 +0530

    fetch api online in jsonplaceholder show all data and also view particular selected data using useEffect , useState & useParam

--- React_js/test_fetch_json_data/src/App.js
+++ React_js/test_fetch_json_data/src/App.js
@@ -1,28 +1,28 @@
-// import React from 'react';
-// // import Navbar from './component/Navbar';
-// import { BrowserRouter as Main , Route,Routes } from 'react-router-dom';
-// import News from './component/News';
+import React from 'react';
+// import Navbar from './component/Navbar';
+import { BrowserRouter as Main , Route,Routes } from 'react-router-dom';
+import News from './component/News';
 
-// function App() {
-//   return (
-//     <Main>
-//     <Routes>
-//           <Route path="/" element={<News />} />
-//           <Route exact path='/:id' element={<News />} />   
-//         </Routes>
-//     </Main>
-//   );
-// }
-// export default App;
-import React from 'react'
-import TableData from './component/TableData'
-
-
-export default function App() {
+function App() {
   return (
-    <div>
-      <h1 className='text-center'>Hello World</h1>
-      <TableData/>
-    </div>
-  )
+    <Main>
+    <Routes>
+          <Route path="/" element={<News />} />
+          <Route exact path='/:id' element={<News />} />   
+        </Routes>
+    </Main>
+  );
 }
+export default App;
+// import React from 'react'
+// import TableData from './component/TableData'
+
+
+// export default function App() {
+//   return (
+//     <div>
+//       <h1 className='text-center'>Hello World</h1>
+//       <TableData/>
+//     </div>
+//   )
+// }
--- React_js/test_fetch_json_data/src/component/News.js
+++ React_js/test_fetch_json_data/src/component/News.js
@@ -1,100 +1,93 @@
-import React ,{ useState ,useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import NewsItems from './NewsItems';
-import { useParams,Link, useLocation } from 'react-router-dom';
+import { useParams, Link, useLocation } from 'react-router-dom';
 
 function News() {
- 
-  
   const [employees, setEmployees] = useState([]);
+  const [employee, setEmployee] = useState([]);
+
+  const params = useParams();
+  const id = params.id;
 
   useEffect(() => {
     async function fetchEmployees() {
       const response = await fetch('https://jsonplaceholder.typicode.com/users');
       const fetchedEmployees = await response.json();
       setEmployees(fetchedEmployees);
+      setEmployee(fetchedEmployees); // Set employee to all employees initially
     }
-
     fetchEmployees();
   }, []);
 
-  let filteredData = employees;
-  const { id } = useParams();
-  if(id)
-  {
-    filteredData = employees.filter(item => item.id === parseInt(id));
-  }
+  useEffect(() => {
+    async function fetchEmployee() {
+      if (id) {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/users?id=${id}`);
+        const fetchedEmployee = await response.json();
+        setEmployee(fetchedEmployee);
+      } else {
+        // Reset employee to all employees if there is no ID in the URL
+        setEmployee(employees);
+      }
+    }
+    fetchEmployee();
+  }, [id, employees]);
 
-  const{pathname}=useLocation();
-
-  const showNavbar=pathname==='/';
-
-
-  
   return (
     <>
-{showNavbar && (
-        <nav className="navbar navbar-expand-lg bg-dark " data-bs-theme="dark">
-          <div className="container-fluid">
-            <Link target="_blank"className="navbar-brand" to="/">
-              Json Data
-            </Link>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                {employees.map((item) => (
-                  <li className="nav-item" key={item.id}>
-                    <Link
-                    target='_blank'
-                      className="nav-link"
-                      to={`/${item.id}`}
-                    >
-                      {item.id}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
+      <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            Json Data
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              {employees.map((item) => (
+                <li className="nav-item" key={item.id}>
+                  <Link className="nav-link" to={`/${item.id}`}>
+                    {item.id}
+                  </Link>
+                </li>
+              ))}
+            </ul>
           </div>
-        </nav>)}
-
-          <h2 className="text-center text-danger fw-bold scroll-text py-3">
-            Json Data Fetching
-          </h2>
-          <div className="container ">
-          <div className="row row-gap-3">
-            {filteredData && filteredData.map((element,index) => {
-                return (
-                  <div className="col-lg-3 col-md-4 col-12" key={index}>
-                    <NewsItems
-                        id={element.id }
-                        name={element.name}
-                        username={element.username}
-                        email={element.email}
-                        address={element.address.city}
-                        phone={element.phone}
-                        website={element.website}
-                    />
-                  </div>
-                );
-              })}
-          </div>
+        </div>
+      </nav>
+      <h2 className="text-center text-danger fw-bold scroll-text py-3">
+        Json Data Fetching
+      </h2>
+      <div className="container ">
+        <div className="row row-gap-3">
+          {employee.map((element, index) => {
+            return (
+              <div className="col-lg-3 col-md-4 col-12" key={index}>
+                <NewsItems
+                  id={element.id}
+                  name={element.name}
+                  username={element.username}
+                  email={element.email}
+                  address={element.address.city}
+                  phone={element.phone}
+                  website={element.website}
+                />
+              </div>
+            );
+          })}
+        </div>
       </div>
-
     </>
-  )
+  );
 }
 
-export default News
+export default News;
